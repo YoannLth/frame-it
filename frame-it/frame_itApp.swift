b8fbc9a6b2654b440aff51d68deeff990b38b31c
Yoann Lathuiliere <ylathuiliere@sqli.com>
2023-07-18 23:54:47 +0200

    Initial Commit

---
+++ frame-it/frame_itApp.swift
@@ -0,0 +1,17 @@
+//
+//  frame_itApp.swift
+//  frame-it
+//
+//  Created by Yoann LATHUILIERE on 18/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct frame_itApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
